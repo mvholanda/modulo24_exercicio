e22cdef3a6bc4099b2074f9162d6dcb044a794ff
Marcus Holanda <mvholanda@gmail.com>
2023-09-22 03:48:50 -0300

    adicionando instancias

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-function Registro(nome, char, caveira, ip) {
+function Registro(nome, char, ip) {
     this.nome = nome
     this.char = char
-    this.caveira = caveira
 
     let _ip = ip 
 
@@ -11,11 +10,31 @@ function Registro(nome, char, caveira, ip) {
 
     this.setIp = function (numeroIp) {
         if (typeof numeroIp === 'number') {
-            return _ip = numeroIp
+            return _ip = `IP: ${numeroIp}`
         }
     }
 }
 
-const registro1 = new Registro('Marcus', 'Caps', 'Server Admin', 12345678)
+function Membro(nome, char, ip) {
+    Registro.call(this, nome, char, ip)
+}
+
+function Movedor(nome, char, ip) {
+    Registro.call(this, nome, char, ip)
+}
+
+function Admin(nome, char, ip) {
+    Registro.call(this, nome, char, ip)
+}
+
+const registro1 = new Membro('Marcus', 'Caps', 12345678)
 console.log(registro1)
 console.log(registro1.getIp())
+
+const registro2 = new Movedor('William', 'Ware', 12344321)
+console.log(registro2)
+console.log(registro2.getIp())
+
+const registro3 = new Admin('Matheus', 'Bara', 43211234)
+console.log(registro3)
+console.log(registro3.getIp())
